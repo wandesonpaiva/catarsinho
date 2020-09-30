f8a4f8c7c9d6b3091551765fbc3cbe7e1963becd
Wandeson Paiva <wandeson.jpaiva@gmail.com>
2020-09-30 12:47:22 -0300

    Add validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,22 @@
 class Project < ApplicationRecord
+
+	validates :title, presence: true
+	validates :description, presence: true
+	validates :due_date, presence: true
+	validates :target, presence: true
+
+	validates :title, length: { maximum: 64 }
+	validates :description, length: { maximum: 512 }
+
+	validates :target, numericality: { greater_than: 0 }
+	validates :target, numericality: { less_than_or_equal_to: 500 }
+
+	validate :due_date_cannot_be_in_the_past_or_30_days_from_today
+
+	def due_date_cannot_be_in_the_past_or_30_days_from_today
+		if due_date.present? and (due_date < Date.today or due_date > Date.today + 30.days)
+			errors.add(:due_date, "can't be in the past or 30 days from today")
+		end
+	end
+
 end
