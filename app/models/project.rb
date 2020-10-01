c743da5789c60a64ad3c2c97974bd5600c0c1d8a
Wandeson Paiva <wandeson.jpaiva@gmail.com>
2020-10-01 11:13:23 -0300

    Adjust layout and add extra data

--- app/models/project.rb
+++ app/models/project.rb
@@ -14,11 +14,24 @@ class Project < ApplicationRecord
 	validate :due_date_cannot_be_in_the_past_or_30_days_from_today
 
 	belongs_to :user
+	has_many :donations
 
 	def due_date_cannot_be_in_the_past_or_30_days_from_today
 		if due_date.present? and (due_date < Date.today or due_date > Date.today + 30.days)
 			errors.add(:due_date, "can't be in the past or 30 days from today")
 		end
+	end
+
+	def donators
+		Donation.joins(:user).where(project_id: self.id).group('users.email').pluck('users.email').to_sentence
+	end
+
+	def total_received
+		total = 0
+		self.donations.each do |donation|
+			total = total + donation.value
+		end
+		total
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :projects do
     resources :donations, only: [:new, :create]
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'projects#index'
 end
